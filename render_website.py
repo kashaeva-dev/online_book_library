98ba08cbe336b03de3167c1aeb37edf770c1e63a
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-09-20 19:23:29 +0300

    Minor changes

--- render_website.py
+++ render_website.py
@@ -12,18 +12,18 @@ def render_website(filepath):
     books_per_page = 16
 
     env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml']),
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(["html", "xml"]),
     )
 
-    template = env.get_template('template.html')
+    template = env.get_template("template.html")
 
-    with open(filepath, 'r', encoding='utf8') as file:
+    with open(filepath, "r", encoding="utf8") as file:
         books_details = json.load(file)
 
     chuncked_books_details = list(chunked(books_details, books_per_page))
 
-    os.makedirs('pages', exist_ok=True)
+    os.makedirs("pages", exist_ok=True)
 
     pages_count = len(chuncked_books_details)
     for index, books_details_chunck in enumerate(chuncked_books_details, start=1):
@@ -33,18 +33,18 @@ def render_website(filepath):
             current_page=index,
             cwdir=os.getcwd(),
         )
-        with open(f'pages/index{index}.html', 'w', encoding='utf8') as file:
+        with open(f"pages/index{index}.html", "w", encoding="utf8") as file:
             file.write(rendered_page)
 
 
 def create_parser():
     parser = argparse.ArgumentParser(
-        prog='Online book library',
+        prog="Online book library",
     )
-    parser.add_argument('filepath',
-                        nargs='?',
+    parser.add_argument("filepath",
+                        nargs="?",
                         help="You can spesify filepath to the books' data in json format",
-                        default='books_details.json',
+                        default="books_details.json",
                         )
 
     return parser
@@ -54,18 +54,15 @@ def main():
     parser = create_parser()
     args = parser.parse_args()
 
-    print(args)
     render_website(args.filepath)
 
     render_website_partial = partial(render_website, args.filepath)
 
     server = Server()
 
-    server.watch(os.path.join(os.getcwd(), 'media/'), render_website_partial)
-    server.watch(os.path.join(os.getcwd(), args.filepath), render_website_partial)
-    server.watch(os.path.join(os.getcwd(), 'template.html'), render_website_partial)
-    server.serve(root='.', port=8080, host='127.0.0.1', default_filename='pages/index1.html')
+    server.watch(os.path.join(os.getcwd(), "template.html"), render_website_partial)
+    server.serve(root=".", port=8080, host="127.0.0.1", default_filename="pages/index1.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
