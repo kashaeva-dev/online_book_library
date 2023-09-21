71a68eea8ed88d68ad18733291f250fcdc9e27c4
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-09-21 18:46:09 +0300

    Fix newlines

--- render_website.py
+++ render_website.py
@@ -10,21 +10,16 @@ from more_itertools import chunked
 
 def render_website(filepath):
     books_per_page = 16
-
     env = Environment(
         loader=FileSystemLoader("."),
         autoescape=select_autoescape(["html", "xml"]),
     )
-
     template = env.get_template("template.html")
-
     with open(filepath, "r", encoding="utf8") as file:
         books_details = json.load(file)
-
     chuncked_books_details = list(chunked(books_details, books_per_page))
 
     os.makedirs("pages", exist_ok=True)
-
     pages_count = len(chuncked_books_details)
     for index, books_details_chunck in enumerate(chuncked_books_details, start=1):
         rendered_page = template.render(
@@ -53,13 +48,9 @@ def create_parser():
 def main():
     parser = create_parser()
     args = parser.parse_args()
-
     render_website(args.filepath)
-
     render_website_partial = partial(render_website, args.filepath)
-
     server = Server()
-
     server.watch(os.path.join(os.getcwd(), "template.html"), render_website_partial)
     server.serve(root=".", port=8080, host="127.0.0.1", default_filename="pages/index1.html")
 
